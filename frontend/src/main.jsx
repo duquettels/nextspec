0709f54bf36728bc40190a14b12994b58842c128
Allen Orozco Lopez <allenorozcro@gmail.com>
2026-09-09 15:03:39 -0400

    Current progress so far

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import "./global.css";
+
+//this will be my main entry point for App.jsx
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
