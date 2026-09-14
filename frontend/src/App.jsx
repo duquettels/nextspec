60574632ac6a84628e34d2e4d0866a5ef72af867
Allen Orozco Lopez <allenorozcro@gmail.com>
2026-09-14 11:23:39 -0400

    added real-time clock and updated placeholder file names

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,19 @@ import {useState, useEffect} from "react";
 
 
 import Sidebar from "./components/Sidebar";
+import RealTimeClock from "./components/RealTimeClock";
 import Dashboard from "./Renders/Dashboard";
 
 export default function App() {
   return (
     <div className="app-shell">
       <Sidebar />
-
-
       <div className="main">
         <div className="header">
           <h1>Welcome back, Consumer!</h1>
-          <div className="profile">
-            Active Rig: <span className="profile-badge">HP OMEN 40L</span>
-          </div>
+          <RealTimeClock />
         </div>
-
-        <Dashboard />
-        
-        
+        <Dashboard />   
       </div>
     </div>
   );
